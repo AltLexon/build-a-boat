c4098f42bacc221e34c87d9c04d6f16b14fcdc84
AltLexon <93941688+AltLexon@users.noreply.github.com>
2023-01-18 17:36:32 -0300

    Delete run.lua

--- run.lua
+++
@@ -1,9 +0,0 @@
-local rbxmSuite = loadstring(game:HttpGetAsync("https://github.com/richie0866/rbxm-suite/releases/latest/download/rbxm-suite.lua"))()
-rbxmSuite.launch("build-a-boat/Build A Boat For Treasure.rbxm", {
-    runscripts = true,
-    deferred = true,
-    nocache = false,
-    nocirculardeps = true,
-    debug = true,
-    verbose = false
-})
